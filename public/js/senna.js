4e652e3dbd3cc84915b52f66a0b242e161ebb881
Sean Packham <sean@seanpackham.com>
2017-11-27 15:01:49 +0000

    Working senna with active menu item

--- public/js/senna.js
+++ public/js/senna.js
@@ -1,26 +1,45 @@
 
 
 function scroll_toc(path) {
-  console.log(path);
+  path = path.indexOf('/docs') == 0? path.substring(4) : path.substring(1);
+  
+  if(path[path.length - 1] == '/') {
+    path = path.substring(0, path.length - 1);
+  }
+  
+  var path = '.' + path.split('/').join(' .');
+  
+  if(window.toc.active) {
+    window.toc.active.removeClass('active');
+  }
+
+  window.toc.active = $(path);
+  window.toc.active.addClass('active');
 }
 
 $(document).ready(function() {
-	scroll_toc(window.location.pathname);
+  window.toc = { active: null };
+  
+  scroll_toc(window.location.pathname);
 
-	var app = new senna.App();
-
-	app.setBasePath('/');
+  var app = new senna.App();
+  app.setBasePath('/');
   // replace html with 'content' id
-	app.addSurfaces('content');
-	app.addRoutes(new senna.Route(/.*/, senna.HtmlScreen));
+  app.addSurfaces('content');
+  app.addRoutes(new senna.Route(/.*/, senna.HtmlScreen));
 
-	app.on('startNavigate', function(event) {
+  app.on('startNavigate', function(event) {
     scroll_toc(event.path)
-	});
+  });
+  
+  app.on('endNavigate', function(event) {
+    var hash = event.path.indexOf('#');
 
-	app.on('endNavigate', function(event) {
-    
-  	var hashPosition = event.path.indexOf('#');
-    console.log(hashPosition);
+  	if (hash !== -1) {
+  		location.hash = path.substr(hash);
+  	}
+    else {
+  		$('#content').scrollTop(0);
+  	}
 	});
 });
