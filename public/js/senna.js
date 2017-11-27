09d8ab1339d5c721b06b132bf826dc43cb6612bb
Sean Packham <sean@seanpackham.com>
2017-11-27 13:54:14 +0000

    Add jquery and our senna init script

--- public/js/senna.js
+++ public/js/senna.js
@@ -1,1 +1,25 @@
 
+
+function scroll_toc(path) {
+  console.log(path);
+}
+
+$(document).ready(function() {
+	scroll_toc(window.location.pathname);
+
+	var app = new senna.App();
+
+	app.setBasePath('/docs');
+	app.addSurfaces('content');
+	app.addRoutes(new senna.Route(/.*/, senna.HtmlScreen));
+
+	app.on('startNavigate', function(event) {
+    scroll_toc(event.path)
+	});
+
+	app.on('endNavigate', function(event) {
+    
+  	var hashPosition = event.path.indexOf('#');
+    console.log(hashPosition);
+	});
+});
