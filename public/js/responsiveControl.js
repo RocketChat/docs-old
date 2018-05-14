47bf79fb6d97b4cc8b4a6af6ebd4b19c69ed38ef
Martin Schoeler <martin.schoeler@rocket.chat>
2018-05-14 17:02:12 -0300

    Move search bar to toc

--- public/js/responsiveControl.js
+++ public/js/responsiveControl.js
@@ -9,45 +9,35 @@ function moveToC(){
     }
 }
 
+function moveSearch(){
+    var viewportWidth = window.innerWidth;
+    if (viewportWidth < 1000) {
+        $('.search.docs').appendTo($('.toc-sidebar'));
+    } else {
+        $('.ui.sidebar').sidebar('hide');
+        $('.search.docs').appendTo($('.toc-wrapper'));
+    }
+}
+
 window.onresize = function () {
+    moveSearch()
     moveToC();
 }
 
+moveSearch()
 moveToC();
 
 
 var burger = document.querySelector(".burger");
-var search = document.querySelector(".search");
-var searchInput = document.querySelector(".search input")
+
 var sidebar = document.querySelector(".sidebar")
 
 burger.addEventListener('click', function (){
     $('.ui.sidebar').sidebar('toggle');
 });
 
-search.addEventListener('click', function () {
-    var viewportWidth = window.innerWidth;
-    if (viewportWidth < 1000) {
-        this.classList.add('active');
-    }
-});
-
-searchInput.addEventListener('blur', function (e) {
-    var viewportWidth = $(window).width();
-    if (viewportWidth < 1000 ) {
-        document.querySelector(".search").classList.remove('active');
-    }
-});
-
 sidebar.addEventListener('click', function () {
     if (event.target.tagName.toLowerCase() === 'a') {
         $('.ui.sidebar').sidebar('hide');
-    }
-});
-
-search.addEventListener('transitionend', function () {
-    var viewportWidth = window.innerWidth;
-    if (viewportWidth < 1000) {
-        searchInput.focus();
     }
 });
